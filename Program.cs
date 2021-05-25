3106718e7391aaefadfd9a9aa66662ffa121b6b2
Thiago-Nascimento <thiago.r.nascimento@hotmail.com>
2021-05-25 08:54:22 -0300

    Exemplo de sobrecarga criado

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ExemploSobrecarga.Classes;
+
+namespace ExemploSobrecarga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f = new Funcionario();
+
+            for (var i = 0; i < f.lista.Length; i++)
+            {
+                Console.WriteLine("Digite um item: ");
+                f.lista[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("");
+            
+            Console.WriteLine("Mostrar sem argumentos.");
+            f.Mostrar();
+
+            Console.WriteLine("Mostrar com argumento tipo 'int'.");
+            f.Mostrar(2);
+
+            Console.WriteLine("Mostrar com argumento tipo 'string'.");
+            f.Mostrar("maçã");
+        }
+    }
+}
